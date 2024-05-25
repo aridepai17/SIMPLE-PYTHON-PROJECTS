5ac6434e1fcf00177ea8c181af6c9d3884e846bb
aridepai17 <advaithdepai26@gmail.com>
2024-05-25 12:12:54 +0530

    Dice rolling Simulator essentials

---
+++ DICE ROLLING SIMULATOR/dice.py
@@ -0,0 +1,20 @@
+import random
+def rolldice():
+    number = random.randint(1, 6)
+    print("You rolled: ", number)
+    
+def main():
+    while True:
+        print("1. Roll the dice")
+        print("2. Exit")
+        choice = int(input("Enter your choice: "))
+        
+        if choice == 1:
+            rolldice()
+        elif choice == 2:
+            break
+        else:
+            print("Invalid choice. Please try again.")
+            
+if __name__ == "__main__":
+    main()
