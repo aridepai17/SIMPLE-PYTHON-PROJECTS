03a6bf9c9c4ee4ebde7ef95f4b405d5e18242136
aridepai17 <advaithdepai26@gmail.com>
2024-06-02 21:52:49 +0530

    Text to Morse Code converter using Python Dictionary

---
+++ ENGLISH TO MORSE CODE CONVERTER/morsecode.py
@@ -0,0 +1,18 @@
+morsecode = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-',
+    '?': '..--..', '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-', ' ': '/'
+}
+
+def englishtomorsecode(message):
+    message = message.upper()
+    mc = [morsecode[char] for char in message if char in morsecode]
+    return ' '.join(mc)
+
+inputtext = input("Enter the message to be converted to Morse Code: ")
+mc = englishtomorsecode(inputtext)
+print(f"Original Message: {inputtext}")
+print(f"Morse Code: {mc}") 
